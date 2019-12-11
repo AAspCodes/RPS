59ec682f54517cdff55a10e802622376b4a5ab83
Anthony Asp <asp.anthony1@gmail.com>
2019-12-11 12:31:09 -0800

    move major control to Game to allow non_static()
    By moving nearly all activity to be directed by Game
    This allows game to be Instantiated and call non static functions.

--- src/main.java
+++ src/main.java
@@ -1,9 +1,20 @@
 
 public class main {
 
-	public static void main(String[] args) {
+	public void main(String[] args) {
 		// TODO Auto-generated method stub
+		UserInput uin = new UserInput();
+		
+		this.greetings();
+		
+		
+		System.out.println(uin.rock_paper_scissors());
 
+	}
+	
+	
+	public void greetings() {
+		System.out.println("Welcome to Rock Paper Scissors by Anthony Asp!");
 	}
 
 }
