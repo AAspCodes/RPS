30be292ea90c5910a036c42e6661abcf9b492a03
Anthony Asp <asp.anthony1@gmail.com>
2019-12-11 13:08:32 -0800

    fill out Game funtionality

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,42 @@
 
 public class Game {
+	UserInput uin;
+	boolean gamestyle;
+	int player_1_score;
+	int player_2_score;
+	
+	public Game() {
+		uin = new UserInput();
+	}
+	
+	public void begin() {
+		this.greetings();
+		gamestyle = uin.computer_or_player();
+		
+		while (true) {
+			this.gameloop();
+		}
+		
+	}
+	
+	
+	private void gameloop() {
+		
+		System.out.print("Player 1");
+		String p1_move = this.uin.rock_paper_scissors();
+		
+		String p2_move;
+		if (this.gamestyle) {
+			//p2_move = get_random_move();
+		} else {
+			System.out.print("Player 2");
+			p2_move = this.uin.rock_paper_scissors();
+		}
+		
+		
+		// compare
+		// Announce winner
+		// add points
+		// repeat
 
 }
