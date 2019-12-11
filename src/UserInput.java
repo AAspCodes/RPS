f8db31a17db499197093204ef0d7db844d89dd0d
Anthony Asp <asp.anthony1@gmail.com>
2019-12-11 12:47:18 -0800

    add UserInput Class to encapsulate User input
    + string_choice: take a prompt, and valid options and
    only return the user's input when their answer is valid.
    
    + rock_paper_scissors: ask user, rock, paper, or scissors and
    return 0, 1, 2 repectively.
    
    + computer_or_player: ask user if they wish to play
    against the computer of another local player and return
    true or false repectively.

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,9 +1,60 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
 
 public class UserInput {
+	Scanner scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public UserInput() {
+		this.scanner = new Scanner(System.in);
 
 	}
 
+	public boolean computer_or_player() {
+
+		String answer = this
+				.string_choice(new String[] { "Do you want to play against Computer or a Person? c/p", "c", "p" });
+
+		if (answer.equals(new String("p"))) {
+			return false;
+
+		} else {
+			return true;
+		}
+	}
+
+	public int rock_paper_scissors() {
+		String answer = this.string_choice(new String[] { "Rock, Paper or Scissors? r/p/s", "r", "p", "s" });
+		// TODO convert if tree to switch case
+		if (answer.equals(new String("r"))) {
+			return 0;
+
+		} else if (answer.equals(new String("p"))) {
+			return 1;
+
+		} else {
+			return 2;
+		}
+	}
+
+	public String string_choice(String[] args) {
+		// prompt, option1, option2, option3...
+		String prompt = args[0];
+		String[] options = Arrays.copyOfRange(args, 1, args.length);
+		List<String> valid_options = Arrays.asList(options);
+
+		String answer = "";
+
+		while (true) {
+			System.out.println(prompt);
+			answer = this.scanner.nextLine();
+
+			if (!(valid_options.contains(answer))) {
+				System.out.println("Unusable answer, try again.");
+			} else {
+				break;
+			}
+		}
+		return answer;
+	}
 }
