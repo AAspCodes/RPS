0050675e5fb8b4299af1b43c3986596f3291b0b2
Anthony Asp <asp.anthony1@gmail.com>
2019-12-11 13:10:51 -0800

    change main to simplify

--- src/main.java
+++ src/main.java
@@ -3,18 +3,7 @@ public class main {
 
 	public void main(String[] args) {
 		// TODO Auto-generated method stub
-		UserInput uin = new UserInput();
-		
-		this.greetings();
-		
-		
-		System.out.println(uin.rock_paper_scissors());
-
+		Game game = new Game();
+		game.begin();
 	}
-	
-	
-	public void greetings() {
-		System.out.println("Welcome to Rock Paper Scissors by Anthony Asp!");
-	}
-
 }
