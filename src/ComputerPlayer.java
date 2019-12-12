16b23a7d6d7be4783df94472eefb8ec5203c7f2b
Anthony Asp <asp.anthony1@gmail.com>
2019-12-12 12:12:35 -0800

    Implement Computer Player

---
+++ src/ComputerPlayer.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.Random;
+
+public class ComputerPlayer {
+	List<String> moves;
+	Random rand;
+
+	public ComputerPlayer() {
+		moves = Arrays.asList("rock", "paper", "scissors");
+		rand = new Random();
+	}
+
+	public String getRandomMove() {
+		String randomMove = this.moves.get(this.rand.nextInt(this.moves.size()));
+		return randomMove;
+	}
+
+}
